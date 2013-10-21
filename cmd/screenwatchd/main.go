f51cb0bbbabb08df3dbbf93595ee6cc7fe90907b
Hank Donnay <hdonnay@gmail.com>
2013-10-21 11:48:27 -0500

    new interface, poll all screens on change events

--- cmd/screenwatchd/main.go
+++ cmd/screenwatchd/main.go
@@ -16,44 +16,79 @@ var (
 	verbose        = flag.Bool("v", false, "Be (very) verbose")
 	logFile        = flag.String("l", "", "File to log to. If empty, log to stderr")
 	daemonize      = flag.Bool("d", false, "Daemonize after starting")
-	positionalArgs = flag.String("p", "DP1:--right-of LVDS1", "Comma separated list of <display>:<position> pairs")
+	positionalArgs = flag.String("p", "card0-DP-1:--right-of LVDS1", "Comma separated list of <sys name>:<position> pairs")
 
-	conn     *dbus.Conn
-	l        *log.Logger
-	xrandr   string
-	s        swatch
-	position = make(map[string][]string)
+	conn   *dbus.Conn
+	l      *log.Logger
+	xrandr string
+	s      swatch
 )
 
 type swatch struct {
 	introspect.Introspectable
-	pos map[string][]string
+	pos   map[string][]string
+	state map[string]bool
 }
 
-func (s swatch) Connect(display string) (bool, *dbus.Error) {
-	args := []string{"--output", display, "--auto"}
-	if _, exist := s.pos[display]; exist {
-		for _, a := range s.pos[display] {
-			args = append(args, a)
+func (s swatch) Change() (bool, *dbus.Error) {
+	for display, _ := range s.pos {
+		var err error
+		var stat bool
+		if stat, err = s.status(display); err != nil {
+			return false, &dbus.Error{Name: "READ_ERR"}
+		}
+		switch stat {
+		case true:
+			args := []string{"--output", mangle(display), "--auto"}
+			if _, exist := s.pos[display]; exist {
+				for _, a := range s.pos[display] {
+					args = append(args, a)
+				}
+			}
+			cmd := exec.Command(xrandr, args...)
+			l.Printf("connecting %s\n", display)
+			if *verbose {
+				l.Println(cmd.Args)
+			}
+			cmd.Run()
+			return cmd.ProcessState.Success(), nil
+		case false:
+			cmd := exec.Command(xrandr, "--output", mangle(display), "--off")
+			l.Printf("disconnecting %s\n", display)
+			if *verbose {
+				l.Println(cmd.Args)
+			}
+			cmd.Run()
+			return cmd.ProcessState.Success(), nil
 		}
 	}
-	cmd := exec.Command(xrandr, args...)
-	l.Printf("connecting %s\n", display)
-	if *verbose {
-		l.Println(cmd.Args)
-	}
-	cmd.Run()
-	return cmd.ProcessState.Success(), nil
+	return false, nil
 }
 
-func (s swatch) Disconnect(display string) (bool, *dbus.Error) {
-	cmd := exec.Command(xrandr, "--output", display, "--off")
-	l.Printf("disconnecting %s\n", display)
-	if *verbose {
-		l.Println(cmd.Args)
+// return changes in connected, disconnected
+func (s swatch) status(dev string) (state bool, err error) {
+	var status string
+	f, err := os.Open(fmt.Sprint("/sys/class/drm/", dev, "/status"))
+	if err != nil {
+		return
 	}
-	cmd.Run()
-	return cmd.ProcessState.Success(), nil
+	_, err = fmt.Fscanf(f, "%s\n", &status)
+	if err != nil {
+		return
+	}
+	switch status {
+	case "connected":
+		state = true
+	case "disconnected":
+		state = false
+	default:
+		break
+	}
+	return
+}
+
+func mangle(s string) string {
+	return strings.Join(strings.Split(s, "-")[1:], "")
 }
 
 func init() {
@@ -84,14 +119,40 @@ func init() {
 	if *verbose {
 		l.Printf("parsing '%s'\n", *positionalArgs)
 	}
-	s = swatch{introspect.NewIntrospectable(screenwatch.Introspect), make(map[string][]string)}
+	s = swatch{
+		introspect.NewIntrospectable(screenwatch.Introspect),
+		make(map[string][]string),
+		make(map[string]bool),
+	}
 	for _, a := range strings.Split(*positionalArgs, ",") {
 		arg := strings.Split(a, ":")
 		if _, exist := s.pos[arg[0]]; exist {
 			l.Printf("duplicate display argument '%s:%s', discarding\n", arg[0], arg[1])
 			continue
 		}
+		if *verbose {
+			l.Printf("watching for %s(%s)\n", arg[0], mangle(arg[0]))
+		}
 		s.pos[arg[0]] = strings.Fields(arg[1])
+	}
+	for d, _ := range s.pos {
+		var status string
+		f, err := os.Open(fmt.Sprint("/sys/class/drm/", d, "/status"))
+		if err != nil {
+			l.Fatalln(err)
+		}
+		_, err = fmt.Fscanf(f, "%s\n", &status)
+		if err != nil {
+			l.Fatalln(err)
+		}
+		switch status {
+		case "connected":
+			s.state[d] = true
+		case "disconnected":
+			s.state[d] = false
+		default:
+			l.Fatalf("unknown status, bailing\n", status)
+		}
 	}
 	if *verbose {
 		fmt.Printf("positional arguments: %v\n", s.pos)
